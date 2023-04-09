a789ab15cbf1aacde57e4d179cc1fde473f56591
Angelo Stramieri <angelo.stramieri@gmail.com>
2023-04-09 16:04:20 +0200

    First custom React component.

--- 02-first-challenge/index.js
+++ 02-first-challenge/index.js
@@ -1,16 +1,17 @@
-// WARN: remember: only one parent element!
-const page = (
-  <div>
-    <img src="./react-logo.png" className="logo" />
-    <h1>Fun facts about React</h1>
-    <ul>
-      <li>Was first released in 2013</li>
-      <li>Was originally created by Jordan Walke</li>
-      <li>Has well over 100K stars on Github</li>
-      <li>Is mantained by Facebook</li>
-      <li>Power thousands of enterprire app, including mobile apps</li>
-    </ul>
-  </div>
-);
+function TempFunc() {
+  return (
+    <div>
+      <img src="./react-logo.png" className="logo" />
+      <h1>Fun facts about React</h1>
+      <ul>
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100K stars on Github</li>
+        <li>Is mantained by Facebook</li>
+        <li>Power thousands of enterprire app, including mobile apps</li>
+      </ul>
+    </div>
+  );
+}
 
-ReactDOM.render(page, document.getElementById("root"));
+ReactDOM.render(<TempFunc />, document.getElementById("root"));
