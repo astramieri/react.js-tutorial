a98d537eab0f6af9ebe207dec47a287dcd1e0a94
Angelo Stramieri <angelo.stramieri@gmail.com>
2023-04-27 08:57:47 +0200

    Added a simple React app.

--- 07-first-hacked-app/src/MyApp.js
+++ 07-first-hacked-app/src/MyApp.js
@@ -1,12 +1,11 @@
 import logo192 from "./images/logo192.png";
 
-export default function App() {
+export default function MyApp() {
     return (
         <div>
              <img src={logo192} alt="logo"></img>
             <h1>Hello, Create React App!</h1>
-        </div>
-        
+        </div>   
     )
 }
 
