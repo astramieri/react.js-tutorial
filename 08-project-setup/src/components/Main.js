c0cc1ccce63d1c5e3b3d165a19d36ad4dbb51d08
Angelo Stramieri <angelo.stramieri@gmail.com>
2023-04-28 07:09:34 +0200

    Challenge completed

---
+++ 08-project-setup/src/components/Main.js
@@ -0,0 +1,11 @@
+//import '../index.css';
+
+function Main() {
+  return (
+    <div className="Main">
+      <h3>Main is here</h3>
+    </div>
+  );
+}
+
+export default Main;
---
+++ 08-project-setup/src/components/Navbar.js
@@ -0,0 +1,11 @@
+//import '../index.css';
+
+function Navbar() {
+  return (
+    <div className="Navbar">
+      <h3>Navbar is here</h3>
+    </div>
+  );
+}
+
+export default Navbar;
