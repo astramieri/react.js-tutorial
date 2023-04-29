229bfd8db798510eab07b491f7eca9aebaa39f57
Angelo Stramieri <angelo.stramieri@gmail.com>
2023-04-29 15:39:18 +0200

    Completed main component.

--- 08-project-setup/src/components/Main.js
+++ 08-project-setup/src/components/Main.js
@@ -1,8 +1,15 @@
 function Main() {
   return (
-    <div className="Main">
-      <h3>Main is here</h3>
-    </div>
+    <main>
+      <h1 className="main-title">Fun facts about Reacts</h1>
+      <ul className="main-list">
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100K stars on GitHub</li>
+        <li>Is maintained by Meta</li>
+        <li>Powers thousands of enterprise app, including mobile apps</li>
+      </ul>
+    </main>
   );
 }
 
