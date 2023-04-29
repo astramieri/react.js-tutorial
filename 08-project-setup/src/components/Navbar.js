07f2f4f214b64e7d7da531f2fa376cad91086522
Angelo Stramieri <angelo.stramieri@gmail.com>
2023-04-29 15:06:47 +0200

    Built the nav component

--- 08-project-setup/src/components/Navbar.js
+++ 08-project-setup/src/components/Navbar.js
@@ -2,10 +2,10 @@ import react_logo from '../images/react_logo.png';
 
 function Navbar() {
   return (
-    <nav className="Navbar">
-      <img src={react_logo} alt="logo" width="60px"></img>
-      <h3>React Facts</h3>
-      <h4>React Course - Project 1</h4>
+    <nav>
+      <img src={react_logo} alt="logo" className="nav-logo"></img>
+      <h3 className="nav-title">React Facts</h3>
+      <h4 className="nav-text">React Course - Project 1</h4>
     </nav>
   );
 }
